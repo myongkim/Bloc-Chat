c9995baf7445b0b288fe7d9da1bc9756cf228a56
Isaac <myong328@gmail.com>
2017-02-08 12:56:48 -0800

    modified

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,16 @@
         this.rooms = Room.all;
     }
 
+
+    this.open = function() {
+    $uibModal.open({
+        templateUrl: 'roomsModal.html',
+        controller: 'ModalInstanceCtrl'
+
+});
+    return this.rooms;
+
+}
     angular
         .module('blocChat')
         .controller('HomeCtrl', ['Room', HomeCtrl]);
